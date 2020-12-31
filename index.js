197c016422a6ce9f7fd5e2dd1f68ff931a32f556
Santos-luis <luissh5@outlook.com>
2020-12-31 17:31:50 +0000

    Replace tabs by spaces

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const axios = require('axios');
 
 try {
   const {
-		env: {
-			SLACK_WEBHOOK: webhookUrl,
-		},
-	} = process;
+    env: {
+      SLACK_WEBHOOK: webhookUrl,
+    },
+  } = process;
 
   const { context: { payload } } = Github;
 
@@ -17,8 +17,8 @@ try {
       requested_reviewers: requestedReviewers,
       title,
       user: {
-				login: authorName,
-				avatar_url: authorAvatar,
+        login: authorName,
+        avatar_url: authorAvatar,
       },
     },
     repository: {
@@ -28,42 +28,42 @@ try {
 
   const reviewersNamesArray = requestedReviewers.map(({ login }) => login);
   const reviewersNames = reviewersNamesArray.join(', ');
-	
-	axios.post(
-		webhookUrl,
-		{
-			'username': 'TAGUS reviewer',
-			'icon_url': 'https://apprecs.org/ios/images/app-icons/256/20/921456160.jpg',
-			'attachments': [
-				{
-					'color': '#36a64f',
-					'pretext': 'Plsssss reviews 🙏',
-					'text': '⠀',
-					'author_name': authorName,
-					'author_icon': authorAvatar,
-					'title': title,
-					'title_link': htmlUrl,
-					'fields': [
-							{
-									'title': 'Repository',
-									'value': `${repoName}`,
-									'short': true,
-							},
-							{
-									'title': 'A short field\'s title',
-									'value': 'A short field\'s value',
-									'short': true,
-							},
-							{
-									'title': 'Reviewers',
-									'value': reviewersNames,
-									'short': false,
-							},
-					],
-				},
-			],
-		},
-	);
+
+  axios.post(
+    webhookUrl,
+    {
+      'username': 'TAGUS reviewer',
+      'icon_url': 'https://apprecs.org/ios/images/app-icons/256/20/921456160.jpg',
+      'attachments': [
+        {
+          'color': '#36a64f',
+          'pretext': 'Plsssss reviews 🙏',
+          'text': '⠀',
+          'author_name': authorName,
+          'author_icon': authorAvatar,
+          'title': title,
+          'title_link': htmlUrl,
+          'fields': [
+            {
+              'title': 'Repository',
+              'value': `${repoName}`,
+              'short': true,
+            },
+            {
+              'title': 'A short field\'s title',
+              'value': 'A short field\'s value',
+              'short': true,
+            },
+            {
+              'title': 'Reviewers',
+              'value': reviewersNames,
+              'short': false,
+            },
+          ],
+        },
+      ],
+    },
+  );
 } catch ({ message }) {
   Core.setFailed(message);
 };
