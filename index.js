33bb69a0ef603b963d249e7fe46927ad5353f881
Santos-luis <luissh5@outlook.com>
2020-12-31 13:56:42 +0000

    Change variables and format code

--- index.js
+++ index.js
@@ -2,48 +2,10 @@ const Core = require('@actions/core');
 const Github = require('@actions/github');
 const Slack = require('node-slack');
 
-const DefaultPRApprovedFormat = `Pull request *{ pull_request.title }* was approved by { review.user.login } :heavy_check_mark:`;
-const DefaultPRChangesRequestedFormat = `Pull request *{ pull_request.title }* was rejected by { review.user.login } :cry:`;
-const DefaultPRReadyForReviewFormat = `:rocket: New PR ready for review! :rocket:\nTitle: *{ pull_request.title }*\nAuthor: { pull_request.user.login }\nURL: { pull_request.html_url }`;
-
-const fillTemplate = (payload, template) => {
-  let message = template;
-  template.match(/\{(.*?)\}/g).forEach(template => {
-    const templateWithoutBrackets = template
-      .replace(/^\{\s?/, "")
-      .replace(/\s?\}$/, "");
-
-    const keys = templateWithoutBrackets.split(".");
-
-    let value = payload;
-    keys.forEach(key => {
-      try {
-        if (value[key]) {
-            value = value[key];
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    });
-    message = message.replace(template, value);
-  });
-  return message;
-};
-
 try {
-  e = process.env;
-  config = {
-    hookUrl: e.SLACK_WEBHOOK,
-    ignoreDrafts: e.IGNORE_DRAFTS || true,
-    pr_approved_format: e.PR_APPROVED_FORMAT || DefaultPRApprovedFormat,
-    pr_ready_for_review_format: e.PR_READY_FOR_REVIEW_FORMAT || DefaultPRReadyForReviewFormat,
-    pr_rejected_format: e.PR_REJECTED_FORMAT || DefaultPRChangesRequestedFormat,
-    username: e.USERNAME || 'ReadyForReviewBot'
-  };
-
-  if (!config.hookUrl) {
-    Core.setFailed("SLACK_WEBHOOK is not set. Set it with\nenv:\n\tSLACK_WEBHOOK: ${{ secrets.SLACK_WEBHOOK }}\n");
-  }
+  const { env } = process;
+  const webhookUrl = env.SLACK_WEBHOOK;
+  const username = env.USERNAME || 'ReadyForReviewBot';
 
   const payload = Github.context.payload;
   const {
@@ -59,43 +21,22 @@ try {
       name: repoName
     }
   } = payload;
-  // console.log(htmlUrl);
-  // console.log(title);
-  // console.log(repoName);
-  // console.log(authorName);
 
-  const reviewersNamesArray = requestedReviewers.map(({ login }) => login);;
+  const reviewersNamesArray = requestedReviewers.map(({ login }) => login);
   const reviewersNames = reviewersNamesArray.join(', ');
 
-    // console.log(payload);
-    // const review = payload.review;
-    // const pr = payload.pull_request;
-    // let message;
+  const slack = new Slack(webhookUrl);
 
-    // if (!review && payload.action !== "ready_for_review" && payload.action !== "opened") {
-    //     return
-    // }
-
-    const slack = new Slack(config.hookUrl);
-
-    // if (review) {
-    //     if (payload.review.state == "approved") {
-    //         message = fillTemplate(payload, config.pr_approved_format);
-    //     } else if (payload.review.state == "changes_requested") {
-    //         message = fillTemplate(payload, config.pr_rejected_format);
-    //     }
-    // } else {
-    //     if (pr.draft && config.ignoreDrafts === true) {
-    //         return
-    //     }
-
-    //     message = fillTemplate(payload, config.pr_ready_for_review_format);
-    // }
-
-    slack.send({
-        text: `htmlUrl: ${htmlUrl}\ntitle: ${title}\nrepoName: ${repoName}\nauthorName: ${authorName}\nreviewersNames: ${reviewersNames}`,
-        username: config.username
-    });
+  slack.send({
+      text: `
+        htmlUrl: ${htmlUrl}
+        title: ${title}
+        repoName: ${repoName}
+        authorName: ${authorName}
+        reviewersNames: ${reviewersNames}
+      `,
+      username
+  });
 } catch (error) {
   Core.setFailed(error.message);
 };
