f8be85c73029689128c3a175ce53275941a6f6b6
Santos-luis <luissh5@outlook.com>
2020-12-31 15:54:36 +0000

    Trying axios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Core = require('@actions/core');
 const Github = require('@actions/github');
-const Slack = require('node-slack');
+const axios = require('axios');
+// const Slack = require('node-slack');
 
 try {
   const { env } = process;
@@ -14,31 +15,64 @@ try {
       requested_reviewers: requestedReviewers,
       title,
       user: {
-        login: authorName
+				login: authorName,
+				avatar_url: authorAvatar,
       }
     },
     repository: {
-      name: repoName
-    }
+      name: repoName,
+    },
   } = payload;
 
   const reviewersNamesArray = requestedReviewers.map(({ login }) => login);
   const reviewersNames = reviewersNamesArray.join(', ');
 
-  const slack = new Slack(webhookUrl);
+  // const slack = new Slack(webhookUrl);
 
-  const text = `
-		htmlUrl: ${htmlUrl}
-		title: ${title}
-		repoName: ${repoName}
-		authorName: ${authorName}
-		reviewersNames: ${reviewersNames}
-  `.replace(
-    /\t/g,
-    ''
-  );
-
-  slack.send({ text, username });
+  // const text = `
+	// 	htmlUrl: ${htmlUrl}
+	// 	title: ${title}
+	// 	repoName: ${repoName}
+	// 	authorName: ${authorName}
+	// 	reviewersNames: ${reviewersNames}
+  // `.replace(
+  //   /\t/g,
+  //   ''
+	// );
+	
+	// 'https://apprecs.org/ios/images/app-icons/256/20/921456160.jpg'
+	// slack.send({ text, username });
+	
+	axios.post(
+		webhookUrl,
+		{
+			username,
+			"icon_ur": "https://apprecs.org/ios/images/app-icons/256/20/921456160.jpg",
+			"attachments": [
+				{
+					"mrkdwn_in": ["text"],
+					"color": "#36a64f",
+					"pretext": "Plsssss reviews 🙏",
+					"author_name": authorName,
+					"author_icon": authorAvatar,
+					"title": title,
+					"title_link": htmlUrl,
+					"fields": [
+							{
+									"title": "A field's title",
+									"value": "This field's value",
+									"short": false
+							},
+							{
+									"title": "A short field's title",
+									"value": "A short field's value",
+									"short": true
+							},
+					],
+				},
+			],
+		},
+	);
 } catch (error) {
   Core.setFailed(error.message);
 };
